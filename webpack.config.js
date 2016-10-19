1a13a1fc323cfafd2bbe736a0b6643db26314072
Narazaka <info@narazaka.net>
2016-10-20 00:18:34 +0900

    double quotes

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
-const webpack = require('webpack');
-const path = require('path');
+const webpack = require("webpack");
+const path = require("path");
 
 module.exports = {
   module: {
@@ -12,33 +12,33 @@ module.exports = {
     loaders: [
       {
         test: /\.ts$/,
-        loader: 'babel!ts',
+        loader: "babel!ts",
         exclude: /node_modules/,
       },
     ],
   },
   entry: {
-    'shiori-loader': './src/lib/shiori-loader.ts',
+    "shiori-loader": "./src/lib/shiori-loader.ts",
   },
   ts: {
     compilerOptions: {
-      rootDir: 'src',
+      rootDir: "src",
       outDir: "dist",
       declarationDir: "dist",
     },
   },
   output: {
     path: path.join(__dirname),
-    filename: 'dist/lib/[name].js',
-    library: 'shioriLoader',
-    libraryTarget: 'umd',
+    filename: "dist/lib/[name].js",
+    library: "shioriLoader",
+    libraryTarget: "umd",
   },
   plugins: [
     new webpack.optimize.DedupePlugin(),
     new webpack.optimize.AggressiveMergingPlugin(),
   ],
-  devtool: 'source-map',
+  devtool: "source-map",
   resolve: {
-    extensions: ['', '.ts', '.js'],
+    extensions: ["", ".ts", ".js"],
   },
 };
