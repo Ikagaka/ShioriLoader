6ac796917174f7ef060276aeb056850c0d5df3a9
Narazaka <info@narazaka.net>
2016-11-06 01:48:35 +0900

    add eslint

--- webpack.config.js
+++ webpack.config.js
@@ -1,44 +1,44 @@
-const webpack = require("webpack");
-const path = require("path");
+const webpack = require('webpack');
+const path = require('path');
 
 module.exports = {
   module: {
     preLoaders: [
       {
         test: /\.ts$/,
-        loader: "tslint",
-      }
+        loader: 'tslint',
+      },
     ],
     loaders: [
       {
         test: /\.ts$/,
-        loader: "babel!ts",
+        loader: 'babel!ts',
         exclude: /node_modules/,
       },
     ],
   },
   entry: {
-    "shiori-loader": "./src/lib/shiori-loader.ts",
+    'shiori-loader': './src/lib/shiori-loader.ts',
   },
   ts: {
     compilerOptions: {
-      rootDir: "src",
-      outDir: "dist",
-      declarationDir: "dist",
+      rootDir: 'src',
+      outDir: 'dist',
+      declarationDir: 'dist',
     },
   },
   output: {
     path: path.join(__dirname),
-    filename: "dist/lib/[name].js",
-    library: "shioriLoader",
-    libraryTarget: "umd",
+    filename: 'dist/lib/[name].js',
+    library: 'shioriLoader',
+    libraryTarget: 'umd',
   },
   plugins: [
     new webpack.optimize.DedupePlugin(),
     new webpack.optimize.AggressiveMergingPlugin(),
   ],
-  devtool: "source-map",
+  devtool: 'source-map',
   resolve: {
-    extensions: ["", ".ts", ".js"],
+    extensions: ['', '.ts', '.js'],
   },
 };
